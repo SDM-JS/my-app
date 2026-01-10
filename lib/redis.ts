5f2b719078a8cd90a432019a01c6c7e89c8cd3a9
SDM-JS <netlify25@gmail.com>
2026-01-10 12:00:27 +0500

    Added some packages

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,3 +1,5 @@
-import { Redis } from "ioredis"
+// import { Redis } from "ioredis"
 
-export const redis = new Redis(process.env.REDIS_URL!)
+import { useClerk } from "@clerk/nextjs";
+
+// export const redis = new Redis(process.env.REDIS_URL!)
