e09b36d816e59aa2131f64ca44d61bfe0a0762f6
Daler <dalerjonsodikov27@gmail.com>
2026-03-25 19:00:05 +0500

    feat: Regenerate Prisma client to incorporate new models and enums after schema updates.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client"
-
 import { PrismaPg } from "@prisma/adapter-pg";
-
+import {PrismaClient} from "../prisma/generated/client"
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
